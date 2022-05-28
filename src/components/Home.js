f124e36679cdc563eaee3a6d475eb68d546df6eb
Alexander Roldan <alexroldan49@gmail.com>
2022-05-28 03:40:05 -0400

    added navbar component and home component. No styling added at this point.

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+
+
+function Home(){
+
+    return(
+        <>
+            <div className="home-containter">
+                {/* <img  /> image will be inserted here */}
+                <div>
+                    <hi className="main-page-header" >Treat Your Furry friend</hi>
+                    <p className="main-page-text" >insert context of application here</p>
+                    <button className="book-appointment" >Book Appointment</button>
+                </div>
+            </div>
+        </>
+    )
+} export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+
+function Navbar(){
+
+    return(
+        <nav className="nav-bar" >
+            <p className="cat-spaw-logo" >Cat Spaw</p>
+            <ul>
+                <li>Home</li>
+                <li>Appointments</li>
+                <li>Cats</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
