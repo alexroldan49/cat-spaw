fad297bcd472c9d294c5720570c9053ed14cf651
Alexander Roldan <alexroldan49@gmail.com>
2022-05-29 05:19:42 -0400

    added cat resting picture to main menu, with styling.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
-
-
+// import catResting from "./images/cat-raesting.jpg"
 function Home(){
 
     return(
         <>
             <div className="home-containter">
-                {/* <img  /> image will be inserted here */}
+                <img className="resting-cat" src={require("./images/cat-resting.jpg")} alt="Cat Resting" />
                 <div>
                     <h1 className="main-page-header" >Treat Your Furry friend</h1>
                     <p className="main-page-text" >insert context of application here</p>
