19f546053ab5c0fc5f8dccd59713a5924a5edcc0
Alexander Roldan <alexroldan49@gmail.com>
2022-06-19 23:34:09 -0400

    added styling to page and corrected bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,16 @@
 function Home(){
 
     return(
-        <>
-            <div className="home-containter">
+        <div className="home">
+            <div className="background-circle"></div>
+            <div className="home-container">
                 <img className="resting-cat" src={require("./images/cat-resting.jpg")} alt="Cat Resting" />
-                <div>
-                    <h1 className="main-page-header" >Treat Your Furry friend</h1>
+                <div className="home-main-context">
+                    <h1 className="main-page-header" >Treat Your Furry Friend</h1>
                     <p className="main-page-text" >insert context of application here</p>
-                    <button className="book-appointment" >Book Appointment</button>
+                    <button className="book-appointment-btn" >Book Appointment</button>
                 </div>
             </div>
-        </>
+        </div>
     )
 } export default Home
