ba9ec53a23d8cc788ec481393d3b677db4e095c3
Alexander Roldan <alexroldan49@gmail.com>
2022-06-27 01:13:32 -0400

    installed react-router-dom and Material UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ function Navbar(){
         <nav className="nav-bar" >
             <p className="cat-spaw-logo" >Cat Spaw</p>
             <ul className="nav-list" >
-                <li><a>Home</a></li>
-                <li><a>Appointments</a></li>
-                <li><a>Cats</a></li>
-                <li><a>About</a></li>
+                <li><a href="#" >Home</a></li>
+                <li><a href="#" >Appointments</a></li>
+                <li><a href="#" >Cats</a></li>
+                <li><a href="#" >About</a></li>
             </ul>
         </nav>
     )
